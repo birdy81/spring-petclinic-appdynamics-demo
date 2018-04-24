8feed4262f8bf862af15b71920a473ebafd1e4e6
Stefan Siegl <sieglst@googlemail.com>
2018-04-24 16:15:18 +0200

    correct lombok problem where the initialization of a field causes spring autowiring to fail.

--- spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/web/OwnerResource.java
+++ spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/web/OwnerResource.java
@@ -51,7 +51,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 class OwnerResource {
 
-    private final OwnerRepository ownerRepository = null;
+    private final OwnerRepository ownerRepository;
 
     private final ExecutorService executor = Executors.newCachedThreadPool();
 
@@ -98,6 +98,8 @@ class OwnerResource {
         try {
 			return future.get();
 		} catch (Exception e) {
+            e.printStackTrace();
+            log.error(e.getMessage());
 			return Collections.emptyList();
 		}
     }
